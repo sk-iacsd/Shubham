4143b8c56d44355229399985a71d8fad841f268d
sk-iacsd <59307699+sk-iacsd@users.noreply.github.com>
2019-12-29 00:11:42 +0530

    Add files via upload

--- 1.js
+++ 1.js
@@ -1,5 +1,7 @@
-var http=require('http');
-http.createServer(function(req,resp){
-res.writeHead(200,{'Content-Type':'text/plain'});
-res.end("Hello Worlddddddd");}).listen(8080);
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}).listen(8080);
 console.log("on port 8080");
